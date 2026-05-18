8a2ceef11756003ca05196ef08bcd2bd952891a6
Diman4iggg <korovin.dima.200614@gmail.com>
2026-05-19 00:02:48 +0300

    Temporarily load exported data on deploy

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [ -f data.json ]; then
+  python manage.py loaddata data.json
+fi
